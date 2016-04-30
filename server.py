f20d412589baba28df1156696039821984a78922
aldilaff <aldilaff@gmail.com>
2016-04-30 17:38:23 -0500

    Added index.html and updated .gitignore to ignore api keys

--- server.py
+++ server.py
@@ -1,1 +1,17 @@
-#
+# -*- coding: utf-8 -*-
+
+
+from flask import Flask,  render_template, request
+import requests
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    return render_template("index.html")
+
+
+@app.route('')
+
+if __name__ == '__main__':
+    app.run()
